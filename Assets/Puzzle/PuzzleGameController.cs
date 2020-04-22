67d90e7e571433a4db947d054fbc1287e1ce4b33
aleks.haler <aleks.haler.work@gmail.com>
2020-04-22 21:15:51 +0300

    finalUI

---
+++ Assets/Puzzle/PuzzleGameController.cs
@@ -0,0 +1,79 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleGameController : MonoBehaviour//Singleton<PuzzleGameController>
+{
+    [SerializeField] private float satisfyingRadius = 0.05f;
+    [SerializeField] private GameObject elementsCollection;
+    [SerializeField] private GameObject fakeElementsCollection;
+    [SerializeField] private float distributionRadius = 0.45f;
+
+    private int elementsCount = 0;
+
+    private Dictionary<GameObject, Vector3> correctTransforms = new Dictionary<GameObject, Vector3>();
+
+    private void Awake()
+    {
+        //foreach (Transform elementTransform in elementsCollection.transform)
+        //{
+        //    correctTransforms.Add(elementTransform.gameObject, elementTransform.localPosition);
+        //    //elementTransform.localPosition = 
+        //    //elementTransform.localPosition = new Vector3(Random.Range(-distributionRadius, distributionRadius), Random.Range(-distributionRadius, distributionRadius), elementTransform.localPosition.z);
+        //}
+
+        //var elements = elementsCollection.t
+
+        for (int i = 0; i < elementsCollection.transform.childCount; i++)
+        {
+            correctTransforms.Add(elementsCollection.transform.GetChild(i).gameObject, elementsCollection.transform.GetChild(i).localPosition);
+            elementsCollection.transform.GetChild(i).localPosition = fakeElementsCollection.transform.GetChild(i).localPosition;
+        }
+
+    }
+
+    public bool CheckPosition(GameObject element)
+    {
+        Transform elementTransform = element.transform;
+
+        Vector3 correctPosition;
+        correctTransforms.TryGetValue(element, out correctPosition);
+
+        if (Vector3.Distance(elementTransform.localPosition, correctPosition) < satisfyingRadius)
+        {
+            elementTransform.localPosition = correctPosition;
+            elementTransform.localEulerAngles = Vector3.zero;
+            elementTransform.gameObject.layer = 0;
+            Rigidbody rb = elementTransform.gameObject.GetComponent<Rigidbody>();
+            Destroy(rb);
+            Collider col = elementTransform.gameObject.GetComponent<BoxCollider>();
+            Destroy(col);
+
+            elementsCount++;
+            Debug.Log(elementsCount);
+            if(elementsCount == 24) 
+            {
+                /*if ((int)Game.current.progress < (int)Progress.Puzzle)
+                {
+                    GlobalGameController.Instance.SaveProgress(Progress.Puzzle);
+                    CloseScene();
+                    GlobalGameController.Instance.ActivateGenerator(3);
+                };*/
+           
+            }
+
+            return true;
+        }
+        //Debug.Log(Vector3.Distance(elementTransform.position, correctPosition));
+        return false;
+    }
+
+    public void CloseScene()
+    {
+        this.gameObject.SetActive(false);
+        //GlobalGameController.Instance.OnGameClose();
+    }
+
+
+
+}
---
+++ Assets/Puzzle/PuzzleUserInput.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Puzzle{
+    public class PuzzleUserInput : MonoBehaviour
+    {
+
+        //[SerializeField] private GameObject textObject;
+
+        private Transform elementTransform;
+        public PuzzleGameController puzzleGameController;
+        private void Update()
+        {
+            //if (Input.touchCount < 1) return;
+            //var touch = Input.GetTouch(0);
+            //var screenPosition = Camera.main.ScreenToViewportPoint(touch.position);
+
+            //RaycastHit hit;
+            //Ray ray = Camera.main.ScreenPointToRay(touch.position);
+
+
+            //if (touch.phase == TouchPhase.Began)
+            //{
+            //        if (Physics.Raycast(ray, out hit, 200, LayerMask.GetMask("Element")))
+            //        {
+            //        textObject.GetComponent<Text>().text = hit.transform.gameObject.name;
+            //        elementTransform = hit.transform;
+            //        }
+            //}
+            //if (touch.phase == TouchPhase.Moved)
+            //{       
+
+            //    if (Physics.Raycast(ray, out hit, 200, LayerMask.GetMask("Surface")))
+            //    {
+            //        if (elementTransform == null) return;
+            //if (PuzzleGameController.Instance.CheckPosition(elementTransform.gameObject))
+            //{
+
+            //    elementTransform = null;
+            //    return;
+            //}
+            //        elementTransform.position = hit.point;
+            //    }
+
+            //}
+
+            //if (touch.phase == TouchPhase.Ended)
+            //{
+            //    elementTransform = null;
+            //}
+            var touch = Input.mousePosition;
+            var screenPosition = Camera.main.ScreenToViewportPoint(touch);
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(touch);
+
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+               
+                if (Physics.Raycast(ray, out hit, 200, LayerMask.GetMask("Element")))
+                {
+                    elementTransform = hit.transform;
+                }
+            }
+            if (Physics.Raycast(ray, out hit, 200, LayerMask.GetMask("Surface")))
+            {
+                print(elementTransform);
+                if (elementTransform == null) return;
+                if(puzzleGameController.CheckPosition(elementTransform.gameObject))
+                {             
+                    elementTransform = null;
+                    return;
+                }
+                elementTransform.position = hit.point;
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                elementTransform = null;
+            }
+
+        }
+
+
+    }
+}
