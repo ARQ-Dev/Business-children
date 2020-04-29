a06ede3a9f021e4e6c7527a7f01e5a49baa3bec2
abyse <ant.alx@mail.ru>
2020-04-29 12:05:21 +0300

    Fixes

---
+++ Assets/Scripts/UI/RotationController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationController : MonoBehaviour
+{
+    [SerializeField]
+    private ScreenOrientation _orientation = ScreenOrientation.Portrait;
+
+    private void Start()
+    {
+        Screen.orientation = _orientation;
+    }
+
+}
